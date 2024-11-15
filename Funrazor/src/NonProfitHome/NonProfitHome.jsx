b85d930866615668250834276d4c839643c2cdf1
Tessa Orozco <torozco6@gatech.edu>
2024-11-14 20:50:01 -0500

    added routing to go to and return from event creation

--- Funrazor/src/NonProfitHome/NonProfitHome.jsx
+++ Funrazor/src/NonProfitHome/NonProfitHome.jsx
@@ -1,13 +1,25 @@
 import './NonProfitHome.css'
 import CreateEvent from '../CreateEvent/CreateEvent'
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 function NonProfitHome() {
 
   return (
-    <div>
-        <p>Home Page</p>
-        <CreateEvent></CreateEvent>
-    </div>
+    <Router>
+        <Switch>
+            <Route exact path='/'>
+                <div>
+                    <p>Home Page</p>
+                    <Link to='/create-event'>
+                        <button>Create event</button>
+                    </Link>
+                </div>
+            </Route>
+            <Route path='/create-event'>
+                <CreateEvent></CreateEvent>
+            </Route>
+        </Switch>
+    </Router>
   )
 }
 
